6f169e5ad11cd8d6be4db3327fb9052e5f240c65
Darius Bualan <mfpjayb@gmail.com>
2022-09-17 01:29:28 +0800

    added generate interface

--- packages/cli/src/commands/generate/generate-interface.ts
+++ packages/cli/src/commands/generate/generate-interface.ts
@@ -1,1 +1,29 @@
-export function generateInterface() {}
+import { basename, resolve } from "path";
+import { fileExistsChecker } from "../../utils/file-exists-checker";
+import { kebabToCamelCase } from "../../utils/kebab-to-camel-case";
+import { logCreate } from "../../utils/log-create";
+import { logError } from "../../utils/log-error";
+import { writeFile } from "../../utils/write-file";
+
+const interfaceData = `export interface <interfaceName> {
+}`;
+
+export function generateInterface(name: string, options: { [key: string]: any; }) {
+    const baseName = basename(name);
+    const fullDirPath = resolve(process.cwd(), name);
+    const fullPath = resolve(`${fullDirPath}.interface.ts`)
+
+    // check if files don't exists
+    // if yes, then throw an error
+    const fileExists = fileExistsChecker(fullPath);
+    if (fileExists) {
+        return logError(`Unable to create new file. ${fullPath} file already exists.`);
+    }
+
+    // if not, create the files
+    const camelCaseName = kebabToCamelCase(`-${baseName}`);
+    const writeData = interfaceData
+        .replace(/<interfaceName>/g, camelCaseName);
+    writeFile(fullPath, writeData);
+    logCreate(fullPath);
+}
