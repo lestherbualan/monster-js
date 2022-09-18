1ec2f5ba5849c88cb7f2ddf5d9c56141b9775b37
Darius Bualan <mfpjayb@gmail.com>
2022-09-18 14:10:07 +0800

    added generate service implementation

--- packages/cli/src/commands/generate/generate-service.ts
+++ packages/cli/src/commands/generate/generate-service.ts
@@ -1,1 +1,37 @@
-export function generateService() {}
+import { basename, resolve } from "path";
+import { fileExistsChecker } from "../../utils/file-exists-checker";
+import { kebabToCamelCase } from "../../utils/kebab-to-camel-case";
+import { logCreate } from "../../utils/log-create";
+import { logError } from "../../utils/log-error";
+import { writeFile } from "../../utils/write-file";
+
+const serviceData = `import { Service } from "@monster-js/core";
+
+@Service(<singletonOption>)
+export class <serviceName>Service { }
+`;
+
+const singleton = `{
+    singleton: true
+}`;
+
+export function generateService(name: string, options: { [key: string]: any; }) {
+    const baseName = basename(name);
+    const fullDirPath = resolve(process.cwd(), 'src/app', name);
+    const fullPath = resolve(`${fullDirPath}.service.ts`)
+
+    // check if files don't exists
+    // if yes, then throw an error
+    if (fileExistsChecker(fullPath)) {
+        return logError(`Unable to create new file. ${fullPath} file already exists.`);
+    }
+
+    // if not, create the files
+    const camelCaseName = kebabToCamelCase(`-${baseName}`);
+    const writeData = serviceData
+        .replace(/<serviceName>/g, camelCaseName)
+        .replace(/<singletonOption>/g, (options.singleton ? singleton : ''));
+
+    writeFile(fullPath, writeData);
+    logCreate(`${fullPath}`);
+}
--- packages/test-app/src/app/services/auth.service.ts
+++ packages/test-app/src/app/services/auth.service.ts
@@ -1,12 +1,4 @@
 import { Service } from "@monster-js/core";
 
-export class HttpService {
-    public url = 'http://localhost:4000';
-}
-
 @Service()
-export class AuthService {
-    public username = 'helloworld';
-
-    constructor(private http: HttpService) { }
-}
+export class AuthService { }
