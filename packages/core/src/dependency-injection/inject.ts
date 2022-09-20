7c99bd442fa2db946de96f5723897673287c49d2
Darius Bualan <mfpjayb@gmail.com>
2022-09-20 19:40:25 +0800

    removed cercular dependency

--- packages/core/src/dependency-injection/inject.ts
+++ packages/core/src/dependency-injection/inject.ts
@@ -1,7 +1,34 @@
 import { ComponentInstance } from "../interfaces/component-instance.interface";
+import { DIDataSource } from "../interfaces/di-data-source.interface";
+import { MonsterWebComponent } from "../interfaces/monster-web-component.interface";
 import { getDataSource } from "./get-data-source";
 import { globalDi } from "./global-di";
-import { processDataSource } from "./process-data-source";
+
+function construct(dataSource: DIDataSource, context: ComponentInstance) {
+    const constructorParams: any[] = Reflect.getMetadata('design:paramtypes', dataSource.target) || [];
+    const instance = new dataSource.target(...constructorParams.map(param => inject(context, param)));
+    if (instance.onReceiveConfig) {
+        instance.onReceiveConfig(dataSource.config);
+    }
+    return instance;
+}
+
+export function processDataSource(dataSource: DIDataSource, context: ComponentInstance) {
+
+    if (dataSource.mock) {
+        return dataSource.mock;
+    }
+
+    if (dataSource.singleton && dataSource.instance) {
+        return dataSource.instance;
+    } else if (dataSource.singleton && !dataSource.instance) {
+        dataSource.instance = construct(dataSource, context);
+        (context.constructor as MonsterWebComponent).dataSource.set(dataSource.target, dataSource);
+        return dataSource.instance;
+    } else {
+        return construct(dataSource, context);
+    }
+}
 
 export function inject<T = any>(context: ComponentInstance, target: T): T {
 
