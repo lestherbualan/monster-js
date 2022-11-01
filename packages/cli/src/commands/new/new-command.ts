4b4b4c304cf87e35ed8f1c7a0f6482ff84b7e3bf
Lesther Bualan <lestherbualan1718@gmail.com>
2022-11-02 01:00:30 +0800

    add --noapplication option in new command

--- packages/cli/src/commands/new/new-command.ts
+++ packages/cli/src/commands/new/new-command.ts
@@ -5,11 +5,13 @@ import { paths } from "../../paths";
 import { fileExistsChecker } from "../../utils/file-exists-checker";
 import { execSync } from 'child_process';
 import { Command } from "commander";
+import chalk from 'chalk';
 
 export function newCommand(program: Command) {
 
     program.command("new <name>")
         .description("Generate a component files")
+        .option("-na, --noapplication","create without monster application", false)
         .action((name: string, options: ObjectInterface) => {
             const destination = resolve(process.cwd(), name);
             const folderExists = fileExistsChecker(destination, `Unable to generate new app. The folder named ${name} already exists.`);
@@ -20,10 +22,25 @@ export function newCommand(program: Command) {
                 if (error) {
                     return console.error(error);
                 }
+                if(options.noapplication){
+                    console.log("\nCreating empty Project ... \n");
+                    console.log("generating " + chalk.green("tsconfig.json") +" file ... \n");
+                    execSync(`cd ${name} && tsc --init`, {stdio: 'inherit'});
+                    console.log('\n');
+                    console.log("generating " + chalk.green("package.json") +" file ... \n");
+                    execSync(`cd ${name} && npm init --yes`, {stdio: 'inherit'});
 
-                console.log("Installing packages ...");
-                execSync(`cd ${name} && npm install`, {stdio: 'inherit'});
-                console.log("Done");
+                    console.log("\n generating " + chalk.green(".gitignore") +" file ...");
+                    execSync(`cd ${name} && echo "" >.gitignore`, {stdio: 'inherit'});
+
+                    console.log(chalk.green("Done!"));
+                    
+                }else{
+                    console.log("Installing packages ...");
+                    execSync(`cd ${name} && npm install`, {stdio: 'inherit'});
+                    console.log(chalk.green("Done!"));
+                }
+                
             });
         });
 }
